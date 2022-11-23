65434a7a0e2727c797d32ebaaf2d256c42eec5c2
Jolie Sameh <Joliesameh@icloud.com>
2022-11-23 16:46:27 +0200

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# pepper = nil
-# cost = 10
-# password = "avada kedavra"
+pepper = nil
+cost = 10
+password = "avada kedavra"
 # encryptedPassword = ::BCrypt::Password.create("#{password}#{pepper}", :cost => cost).to_s
 
-# User.insert!([
+User.insert!([
   
-#   {
-#     name: "Dumbledore the great",
-#     email: "Dumbledore@great.com",
-#     # password: "avada kedavra",
-#     # password_confirmation: "avada kedavra",
-#     encrypted_password: encryptedPassword,
-#     role: "1",
-#     bio: "",
-#     house: "",
-#     birthday: "1999-7-20"
-#   }
-# ])
+  {
+    name: "Dumbledore the great",
+    email: "Dumbledore@great.com",
+    password: "avada kedavra",
+    password_confirmation: "avada kedavra",
+    # encrypted_password: encryptedPassword,
+    role: "1",
+    bio: "",
+    house: "",
+    birthday: "1999-7-20"
+  }
+])
 
 
 # user = User.new
 
 # user.name = "Dumbledore the great"
 # user.email = "Dumbledore@great.com"
-# user.encrypted_password = encryptedPassword
+# user.encrypted_password = "avada kedavra"
 # # user.password = "avada kedavra"
 # # user.password_confirmation = "avada kedavra"
-# puts user.encrypted_password 
 # user.role= "1"
 # user.bio= ""
 # user.house= ""
 # user.birthday= "1999-7-20"
 # user.save(validate: false)
-# puts user.inspect
